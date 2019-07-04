8fafe6e7f4d325a161938f7d7b749764a4ef6f25
Archaeologist03 <omniiknight@hotmail.com>
2019-07-04 13:37:14 +0200

    Initial prettier, eslint, pre-commit hook, TS, CircleCi.. configuration.

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Logo.module.scss';
+
+const Logo = () => {
+  const a: number = 5;
+
+  return <div className={styles.logoStyle}>Hi there</div>;
+};
+
+export default Logo;
